23834d487b57ea6a0f3ea2dd488a60db6830e2ff
Santoshkumar <santoshkumarbh2001@gmail.com>
2025-03-15 22:09:01 +0530

    useReducer

--- Snagam_Mukherjee/react-project/src/components/use-reducer-example.jsx
+++ Snagam_Mukherjee/react-project/src/components/use-reducer-example.jsx
@@ -8,31 +8,54 @@ export default function UseReducerExample(){
         changeTextStylesFlag:false,
     }
     
+    // const HIDE_TEXT='HIDE_TEXT';
+    // const SHOW_TEXT='SHOW_TEXT';
+    // const CHANGE_EXT_STYLE='CHANGE_TEXT_STYLE';
     
 
     function reducer(state,action){
         switch (action.type) {
-            case value:
-                
-               return state;
+            case 'HIDE_TEXT':
+       
+                return {
+                    ...state,
+                    showTextFlag:false
+                };
+            
+
+            case 'SHOW_TEXT':
+                return {
+                    ...state,
+                    showTextFlag:true
+                };
+
+            
+            case 'CHANGE_TEXT_STYLE':
+                return {
+                    ...state,
+                    changeTextStylesFlag:!state.changeTextStylesFlag
+                };
         
             default:
                 return state;
         }
     }
 
-    const HIDE_TEXT='HIDE_TEXT';
-    const SHOW_TEXT='SHOW_TEXT';
+
     
     const [state,dispatch] = useReducer(reducer,initialState);
-    // console.log(state);
+    console.log(state);
     
     return(
+        
         <div>
-            <h2>Use Reducer Hook Example</h2>
+            {
+                state?.showTextFlag ? <h2 style={{backgroundColor:state?.changeTextStylesFlag ? 'red' :'blue'}}>Use Reducer Hook Example</h2>:null
+            }
+            
             <button onClick={()=>dispatch({type:'HIDE_TEXT'})}>Hide Text</button>
-            <button>Show Text</button>
-            <button>Toggle Text</button>
+            <button onClick={()=>dispatch({type:'SHOW_TEXT'})}>Show Text</button>
+            <button onClick={()=>dispatch({type:'CHANGE_TEXT_STYLE'})}>Toggle Text</button>
         </div>
     );
 }
